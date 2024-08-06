b9f1cf47920729e9013bf12d730031f0e8d9237f
ABDELMAWLASASSIOUI <sasiouiabdelmola@gmail.com>
2024-08-06 21:40:58 +0200

    the first day of project

---
+++ src/main/java/E_Learning_Platforme/ELearningPlatformeApplication.java
@@ -0,0 +1,13 @@
+package E_Learning_Platforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningPlatformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningPlatformeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/E_Learning_Platforme/ELearningPlatformeApplicationTests.java
@@ -0,0 +1,13 @@
+package E_Learning_Platforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningPlatformeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
